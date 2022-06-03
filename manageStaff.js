953f650375eedb15f2bb7e284b29b95acb1c9082
Hannah Schmidt <hannah.schmidt@posteo.de>
2022-06-03 13:11:09 +0200

    staff functions added

--- manageStaff.js
+++ manageStaff.js
@@ -1,9 +1,10 @@
 
-students=[ { id: "576465", fname: "Gina", lname: "Linetti", dob: "1999-02-02", gender: "female", dep: "fb2", email: "576465@htw-berlin.de", join: "2015-02-02" },{ id: "573288", fname: "Terry", lname: "Jeffords", dob: "1999-02-02", gender: "male", dep: "fb2", email: "576468@htw-berlin.de", join: "2015-02-02" },{ id: "57439", fname: "Max", lname: "Mustermann", dob: "1963-07-20", gender: "diverse", dep: "fb3", email: "s058439@htw-berlin.de", join: "2015-12-13" },{ id: "57439", fname: "Jane", lname: "Doe", dob: "1977-07-20", gender: "diverse", dep: "fb4", email: "s043563@htw-berlin.de", join: "2015-09-13" }, { id: "57437", fname: "Janette", lname: "Dodo", dob: "1977-07-20", gender: "male", dep: "fb1", email: "s0576432@htw-berlin.de", join: "2015-04-13" }];
+staff=[ { id: "576465", fname: "Gina", lname: "Linetti", dob: "1999-02-02", gender: "female", dep: "fb2", email: "576465@htw-berlin.de", join: "2015-02-02" },{ id: "573288", fname: "Terry", lname: "Jeffords", dob: "1999-02-02", gender: "male", dep: "fb2", email: "576468@htw-berlin.de", join: "2015-02-02" },{ id: "57439", fname: "Max", lname: "Mustermann", dob: "1963-07-20", gender: "diverse", dep: "fb3", email: "s058439@htw-berlin.de", join: "2015-12-13" },{ id: "57439", fname: "Jane", lname: "Doe", dob: "1977-07-20", gender: "diverse", dep: "fb4", email: "s043563@htw-berlin.de", join: "2015-09-13" }, { id: "57437", fname: "Janette", lname: "Dodo", dob: "1977-07-20", gender: "male", dep: "fb1", email: "s0576432@htw-berlin.de", join: "2015-04-13" }];
 
-function studenttable(){
+function stafftable(){
+    console.log("ahahaaaa")
     const stable=document.getElementById("stable")
-    for(var i=0; i<students.length; i++) { 
+    for(var i=0; i<staff.length; i++) { 
         var row = stable.insertRow(1);
         var cell8 = row.insertCell(-1);
         var cell1 = row.insertCell(0);
@@ -13,36 +14,35 @@ function studenttable(){
         var cell5 = row.insertCell(4);
         var cell6 = row.insertCell(5);
         var cell7 = row.insertCell(6);
-        cell1.innerHTML = students[i].id;
-        cell2.innerHTML = students[i].fname;
-        cell3.innerHTML = students[i].lname;
-        cell4.innerHTML = students[i].dob;
-        cell5.innerHTML = students[i].gender;
-        cell6.innerHTML = students[i].dep;
-        cell7.innerHTML = students[i].email;
-        cell8.innerHTML = students[i].join;
-        students[i].element=row;
+        cell1.innerHTML = staff[i].id;
+        cell2.innerHTML = staff[i].fname;
+        cell3.innerHTML = staff[i].lname;
+        cell4.innerHTML = staff[i].dob;
+        cell5.innerHTML = staff[i].gender;
+        cell6.innerHTML = staff[i].dep;
+        cell7.innerHTML = staff[i].email;
+        cell8.innerHTML = staff[i].join;
+        staff[i].element=row;
     }  
 
 };
 
 function department() {
-    students.forEach(student => student.element.style.display ='')
+    staff.forEach(staff => staff.element.style.display ='')
    let filter = document.getElementById("departmentfilter").value;
    if(filter!='nothing'){
-    students.filter(student => student.dep!=filter).forEach(student => student.element.style.display ='none');
+    staff.filter(staff => staff.dep!=filter).forEach(staff => staff.element.style.display ='none');
    }
     filter = document.getElementById("semester").value;
     if(filter!='nothing'){
-     students.filter(student => szn(student)!=filter).forEach(student => student.element.style.display ='none');
+        staff.filter(staff => szn(staff)!=filter).forEach(staff => staff.element.style.display ='none');
     }
 };
 
 
-function szn(student){
-    var arr = student.join.split('-');
+function szn(member){
+    var arr = member.join.split('-');
     if(arr[1]>3 && arr[1]<10){
-        console.log()
         return "summer";
     }else{
         return "winter";
@@ -83,20 +83,20 @@ function checkForm() {
     }
 };
 
-function addStudent() {
-    let student = {
-        id: document.getElementById("adStudent_Id").value,
+function addStaff() {
+    let member = {
+        id: document.getElementById("adStaff_Id").value,
         fname: document.getElementById("adFirst_Name").value,
         lname: document.getElementById("adLast_Name").value,
         dob: document.getElementById("dob").value,
         gender: document.querySelector('input[name="gender"]:checked').value, 
         dep: document.querySelector('#department').value,
-        email: document.getElementById("adStudent_email").value,
-        join: document.getElementById("adStudent_join").value
+        email: document.getElementById("adStaff_email").value,
+        join: document.getElementById("adStaff_join").value
     }
         const stable=document.getElementById("stable")
         var row = stable.insertRow(1);
-        student.element=row;
+        member.element=row;
         var zell8 = row.insertCell(-1);
         var zell1 = row.insertCell(0);
         var zell2 = row.insertCell(1);
@@ -105,16 +105,14 @@ function addStudent() {
         var zell5 = row.insertCell(4);
         var zell6 = row.insertCell(5);
         var zell7 = row.insertCell(6);
-        zell1.innerHTML = student.id;
-        zell2.innerHTML = student.fname;
-        zell3.innerHTML = student.lname;
-        zell4.innerHTML = student.dob;
-        zell5.innerHTML = student.gender;
-        zell6.innerHTML = student.dep;
-        zell7.innerHTML = student.email;
-        zell8.innerHTML = student.join;
-    students.push(student)
+        zell1.innerHTML = member.id;
+        zell2.innerHTML = member.fname;
+        zell3.innerHTML = member.lname;
+        zell4.innerHTML = member.dob;
+        zell5.innerHTML = member.gender;
+        zell6.innerHTML = member.dep;
+        zell7.innerHTML = member.email;
+        zell8.innerHTML = member.join;
+        staff.push(member)
 }
-
-
 
